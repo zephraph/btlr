d5d04801b7c215ce5565eb6b16cfb17e4191edaa
Justin Bennett <jbennett@scrippsnetworks.com>
2017-03-25 10:38:21 -0400

    Update example service

--- lib/services/jira.ts
+++ lib/services/jira.ts
@@ -5,9 +5,7 @@ const jira = new JiraApi({
 	protocol: 'https',
 	host: 'scrippsnetworks.atlassian.net',
 	apiVersion: '2',
-	strictSSL: true,
-	username: '162885',
-	password: keytar.getPassword('btlr.jira')
+	strictSSL: true
 });
 
 export default jira;
